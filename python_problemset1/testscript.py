7fbd3959cc077cdc461b220654aa929fa123fe01
admin <admin@pfb01.cshl.edu>
2017-10-17 11:19:53 -0400

    commented script

--- python_problemset1/testscript.py
+++ python_problemset1/testscript.py
@@ -2,6 +2,7 @@
 
 import sys
 
+#Sample script to read arguments and print some things about the user
 print("My name is" , sys.argv[1])
 print("My favourite colour is", sys.argv[2])
 print("I like", sys.argv[3])
