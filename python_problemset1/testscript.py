7c0b0083bdd67d1f7c0d96d4f627d05975703915
admin <admin@pfb01.cshl.edu>
2017-10-17 11:16:10 -0400

    new files added for first python problem set

---
+++ python_problemset1/testscript.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+
+print("My name is" , sys.argv[1])
+print("My favourite colour is", sys.argv[2])
+print("I like", sys.argv[3])
+print("My favourite animal is an", sys.argv[4])
