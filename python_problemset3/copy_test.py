81f06d08bcb1c7cd5216058e2bcc58f01815058c
admin <admin@pfb01.cshl.edu>
2017-10-18 12:12:57 -0400

    Updated copy_test.py

--- python_problemset3/copy_test.py
+++ python_problemset3/copy_test.py
@@ -2,14 +2,14 @@
 
 # List copying methods
 
-list = ['a','bb','ccc']
-list_copy = list
-print('List copy method 1', list)
+my_list = ['a','bb','ccc']
+list_copy = my_list
+print('List copy method 1', my_list)
 list_copy.append('dddd')
-print('Appended list copy', list)
+print('Appended list copy', my_list)
 
-list2 = ['a','bb','ccc']
-list2_copy = list2.copy()
-print('List copy method 2', list2)
+my_list2 = ['a','bb','ccc']
+list2_copy = my_list2.copy()
+print('List copy method 2', my_list2)
 list2_copy.append('dddd')
-print('Appended list copy', list2)
+print('Appended list copy', my_list2)
